a1cd1b2db1a4801c7a8faf5c351adb519221cc98
Weison_Zhong <jmzws@qq.com>
2023-01-29 16:02:57 +0800

    add udemy flutter course

---
+++ udemy/flutter_app/android/app/src/main/kotlin/weison/cn/flutter_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package weison.cn.flutter_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ udemy/i_am_rich/android/app/src/main/kotlin/WeisonZhong/cn/i_am_rich/MainActivity.kt
@@ -0,0 +1,6 @@
+package WeisonZhong.cn.i_am_rich
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
